e15ccdd627848356bf1e47d4f7ed4971abd0ebf3
ejweber <webericjames@gmail.com>
2018-08-10 21:33:51 -0500

    Started work on PaintView class

---
+++ app/src/main/java/com/weberster/whiteboard/PaintView.java
@@ -0,0 +1,31 @@
+package com.weberster.whiteboard;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.MaskFilter;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class PaintView extends View {
+
+    public static int BRUSH_SIZE = 20;
+    public static final int DEFAULT_COLOR = Color.RED;
+    public static final int DEFAULT_BG_COLOR = Color.WHITE;
+    private static final float TOUCH_TOLERANCE = 4;
+    private float mX, mY;
+    private Path mPath;
+    private Paint mPaint;
+    private ArrayList<FingerPath> paths = new ArrayList<>();
+    private int strokeWidth;
+    private boolean emboss;
+    private boolean blur;
+    private MaskFilter mEmboss;
+    private MaskFilter mBlur;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Paint mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+}
