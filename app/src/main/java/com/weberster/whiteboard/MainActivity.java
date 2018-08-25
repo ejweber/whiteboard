88ac56beecb86542ded12c9d355664360ec9e4ad
ejweber <webericjames@gmail.com>
2018-08-24 19:25:58 -0500

    Finished establishing very basic functionality

--- app/src/main/java/com/weberster/whiteboard/MainActivity.java
+++ app/src/main/java/com/weberster/whiteboard/MainActivity.java
@@ -2,12 +2,48 @@ package com.weberster.whiteboard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+
+    private PaintView paintView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        paintView = findViewById(R.id.paint_view);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.normal:
+                paintView.normal();
+                return true;
+            case R.id.emboss:
+                paintView.emboss();
+                return true;
+            case R.id.blur:
+                paintView.blur();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
